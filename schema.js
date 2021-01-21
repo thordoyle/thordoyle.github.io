66428d2f00ab00bda79d2c91acd5e65a2b248abd
thordoyle <75585705+thordoyle@users.noreply.github.com>
2021-01-21 20:16:45 +0100

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,172 @@
+const json = {
+  "days": [
+    { // 0
+      "name":"Söndag"
+    },
+    { // 1
+      "name": "Måndag",
+      "classes": [
+        {
+          "name": "Engelska",
+          "start": "08:20",
+          "end": "09:20"
+        },
+        {
+          "name": "Idrott",
+          "start": "09:35",
+          "end": "10:25"
+        },
+        {
+          "name": "NO",
+          "start": "10:45",
+          "end": "11:35"
+        },
+        {
+          "name": "SO",
+          "start": "12:30",
+          "end": "13:45"
+        },
+        {
+          "name": "Svenska",
+          "start": "14:10",
+          "end": "15:10"
+        }
+      ]
+    },
+    { // 2
+      "name": "Tisdag",
+      "classes": [
+        {
+          "name": "Franska",
+          "start": "08:20",
+          "end": "09:10"
+        },
+        {
+          "name": "Matematik",
+          "start": "09:20",
+          "end": "10:10"
+        },
+        {
+          "name": "NO",
+          "start": "10:20",
+          "end": "11:10"
+        },
+        {
+          "name": "Idrott",
+          "start": "12:15",
+          "end": "13:05",
+        },
+        {
+          "name": "Bild",
+          "start": "13:25",
+          "end": "14:10",
+        },
+        {
+          "name": "Svenska",
+          "start": "14:20",
+          "end": "15:20"
+        }
+      ]
+    },
+    { // 3
+      "name": "Onsdag",
+      "classes": [
+        {
+          "name": "Hemkunskap",
+          "start": "08:20",
+          "end": "09:35"
+        },
+        {
+          "name": "Matematik",
+          "start": "09:45",
+          "end": "10:35"
+        },
+        {
+          "name": "Språk",
+          "start": "10:45",
+          "end": "11:35"
+        },
+        {
+          "name": "Svenska",
+          "start": "12:30",
+          "end": "13:30"
+        },
+        {
+          "name": "SO",
+          "start": "13:40",
+          "end": "14:55"
+        },
+        {
+          "name": "Musik",
+          "start": "15:05",
+          "end": "15:55"
+        }
+      ]
+    },
+    { // 4
+      "name": "Torsdag",
+      "classes": [
+        {
+          "name": "Matematik",
+          "start": "09:30",
+          "end": "10:20"
+        },
+        {
+          "name": "Språk",
+          "start": "10:35",
+          "end": "11:25"
+        },
+        {
+          "name": "Idrott",
+          "start": "12:15",
+          "end": "13:05"
+        },
+        {
+          "name": "Mentorstid",
+          "start": "13:15",
+          "end": "13:45"
+        }
+      ]
+    },
+    { // 5
+      "name": "Fredag",
+      "classes": [
+        {
+          "name": "NO",
+          "start": "08:20",
+          "end": "09:20"
+        },
+        {
+          "name": "SO",
+          "start": "09:30",
+          "end": "10:30"
+        },
+        {
+          "name": "Teknik",
+          "start": "10:40",
+          "end": "11:40"
+        },
+        {
+          "name": "Matematik",
+          "start": "12:30",
+          "end": "13:20"
+        },
+        {
+          "name": "Engelska",
+          "start": "13:35",
+          "end": "14:30"
+        },
+        {
+          "name": "Slöjd",
+          "start": "14:45",
+          "end": "15:55"
+        }
+      ]
+    },
+    { // 6
+      "name":"Lördag"
+    }
+  ]
+}
+
+export default json
